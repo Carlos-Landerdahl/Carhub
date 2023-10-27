469ef6f3e89be6d60373400333dcbd5b6d569a37
Bruno Chaves <chavesbruno699@gmail.com>
2023-10-27 17:29:34 -0300

    feature/body-design

---
+++ src/components/global/mainBody/index.js
@@ -0,0 +1,18 @@
+'use client';
+
+import React from 'react';
+import { Box } from '@mui/material';
+
+function MainBody() {
+    return (
+        <Box sx={{
+            backgroundColor: '#fcba03',
+            width: '100%',
+            height: '100%',
+        }}>
+        </Box>
+    );
+};
+
+export default MainBody;
+
