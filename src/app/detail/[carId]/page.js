27776f3e2f85c5cffeae515287578c18b5f82aee
Carlos-Landerdahl <carloslanderdahl27@gmail.com>
2023-11-07 10:44:56 -0300

    feat: car features

--- src/app/detail/[carId]/page.js
+++ src/app/detail/[carId]/page.js
@@ -1,14 +1,52 @@
 'use client';
 
 import React from 'react';
-import dataJson from '@/data.json';
-import { Box, Button, Container, Typography, Grid, Card, CardMedia, CardContent, ButtonBase, Paper } from '@mui/material';
-import {ArrowBackIos, CarRental} from '@mui/icons-material';
 import Link from 'next/link';
+import {
+  Box,
+  Button,
+  Container,
+  Typography,
+  Card,
+  CardMedia,
+  CardContent,
+  Paper,
+  Divider,
+} from '@mui/material';
+import {
+  ArrowBackIos,
+  Beenhere,
+  CarRental,
+  Luggage,
+  Speed,
+  AirlineSeatReclineExtra as AirlineSeatReclineExtraIcon,
+  DirectionsCar as DirectionsCarIcon,
+} from '@mui/icons-material';
 import theme from '@/styles/theme';
+import dataJson from '@/data.json';
 
-export default function DetailCar({ params }) {
-  const { carId } = params;
+const infoCardStyle = {
+  color: theme.palette.text.main,
+  background: theme.palette.background.lightBlue,
+  border: `1px solid ${theme.palette.background.button}`,
+  borderRadius: '8px',
+  p: '5px',
+  mt: '8px',
+  maxWidth: '150px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'start',
+  gap: '5px',
+};
+
+const InfoCard = ({ icon, children }) => (
+  <Typography variant="body2" sx={infoCardStyle}>
+    {icon}
+    {children}
+  </Typography>
+);
+
+export default function DetailCar({ params: { carId } }) {
   const carDetails = dataJson.recommends.find((car) => car.id === parseInt(carId, 10));
 
   if (!carDetails) {
@@ -29,18 +67,8 @@ export default function DetailCar({ params }) {
         justifyContent: 'center',
         alignItems: 'center',
       }}
-      >
-      <Container
-        maxWidth="lg"
-      >
-        <Typography
-          variant="h5"
-          sx={{
-            color: theme.palette.text.text,
-            fontWeight: 'bold',
-          }} gutterBottom>
-          Detalhes do Carro - {carDetails.brand} {carDetails.model}
-        </Typography>
+    >
+      <Container maxWidth="lg">
         <Paper
           elevation={3}
           sx={{
@@ -71,45 +99,63 @@ export default function DetailCar({ params }) {
               Categoria: {carDetails.category}
             </Typography>
             <Button
-              startIcon={<ArrowBackIos/>}
+              startIcon={<ArrowBackIos />}
               sx={{ color: theme.palette.background.button, fontWeight: 'bold' }}
             >
               Voltar
             </Button>
           </Link>
-          <Card sx={{ display:"flex", borderRadius: "0"}}>
+          <Card sx={{ display: 'flex', borderRadius: '0' }}>
             <CardMedia
               component="img"
               alt={`${carDetails.brand} ${carDetails.model}`}
               height="auto"
               image={carDetails.image}
             />
-            <CardContent sx={{
-              display: 'flex',
-              flexDirection: "column",
-              justifyContent: "space-between"
-            }}>
+            <CardContent
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'space-between',
+              }}
+            >
               <Box component="div">
-                <Typography variant="h6" gutterBottom>
-                  Descrição
+                <Typography variant="h6" gutterBottom sx={{ fontWeight: 'bold' }}>
+                  Descrição - {carDetails.brand} {carDetails.model}
                 </Typography>
-                <Typography variant="body1">
-                  {carDetails.description}
-                </Typography>
-                <Typography variant="h6" gutterBottom>
-                  Ano: {carDetails.year}
-                </Typography>
-                <Typography variant="h6" gutterBottom>
-                  Preço por Dia:  R$ {carDetails.price_per_day.toFixed(2)}
+                <Typography variant="body1">{carDetails.description}</Typography>
+                <Box sx={{ mb: '8px' }}>
+                  <InfoCard icon={<Beenhere />}>Ano: {carDetails.year}</InfoCard>
+                  <InfoCard icon={<AirlineSeatReclineExtraIcon />}>2 lugares</InfoCard>
+                  <InfoCard icon={<DirectionsCarIcon />}>Automático</InfoCard>
+                  <InfoCard icon={<Luggage />}>mala pequena</InfoCard>
+                  <InfoCard icon={<Speed />}>Quilometragem ilimitada</InfoCard>
+                </Box>
+                <Divider sx={{ background: theme.palette.background.secondary, opacity: '0.6' }} />
+                <Typography
+                  variant="body1"
+                  gutterBottom
+                  align="center"
+                  sx={{
+                    color: theme.palette.text.price,
+                    background: theme.palette.background.lightGreen,
+                    border: '1px solid #0a8526',
+                    borderRadius: '8px',
+                    p: '5px',
+                    mt: '8px',
+                    maxWidth: '200px',
+                  }}
+                >
+                  Preço por Dia: <strong>R$ {carDetails.price_per_day.toFixed(2)}</strong>
                 </Typography>
               </Box>
-              <Button 
-                variant="contained" 
-                color="primary" 
-                startIcon={<CarRental/>} 
+              <Button
+                variant="contained"
+                color="primary"
+                startIcon={<CarRental />}
                 disabled={!carDetails.available}
               >
-                {carDetails.available ? "Alugar Agora" : "Indisponível"}
+                {carDetails.available ? 'Alugar Agora' : 'Indisponível'}
               </Button>
             </CardContent>
           </Card>
